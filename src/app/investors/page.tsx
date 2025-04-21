6e25760e757c539ead5514353ccc40fcd1bd9f0e
puntocel <alex.buntrock04061989@gmail.com>
2025-04-21 15:30:36 -0400

    address

--- src/app/investors/page.tsx
+++ src/app/investors/page.tsx
@@ -5,7 +5,7 @@ import InvestorPage from './InvestorPage';
 import { Suspense } from 'react';
 
 console.log(process.env.NEXT_PUBLIC_INVESTOR_ID)
-const INVESTOR_ID = process.env.NEXT_PUBLIC_INVESTOR_ID || '5b40fef7-d9c5-4752-b914-e298a7e89715';
+const INVESTOR_ID = '5b40fef7-d9c5-4752-b914-e298a7e89715' || process.env.NEXT_PUBLIC_INVESTOR_ID;
 
 export default function () {
   const investorId = INVESTOR_ID
